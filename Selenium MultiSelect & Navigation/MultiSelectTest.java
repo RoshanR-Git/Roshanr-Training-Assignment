dd061e62a942ae12ca8322a6890484e1e7fd731d
rroshan200431@gmail.com <rroshan200431@gmail.com>
2025-12-14 12:05:30 +0530

    Selenium

---
+++ Selenium MultiSelect & Navigation/MultiSelectTest.java
@@ -0,0 +1,57 @@
+package demo;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class MultiSelectTest {
+    @Test
+    public void selectDropdownOption() {
+    	 WebDriver driver = new ChromeDriver();
+         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+         driver.manage().window().maximize();
+         driver.get("https://demo.mobiscroll.com/select/multiple-select");
+
+         WebElement input = wait.until(
+                 ExpectedConditions.presenceOfElementLocated(By.id("multiple-select-input"))
+         );
+         input.sendKeys(Keys.ENTER);
+
+         wait.until(ExpectedConditions.presenceOfElementLocated(
+                 By.cssSelector(".mbsc-popup-anchored")
+         ));
+
+         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
+                 By.cssSelector(".mbsc-scroller-wheel-item[role='option']")
+         ));
+
+         List<WebElement> options = driver.findElements(
+                 By.cssSelector(".mbsc-scroller-wheel-item[role='option']")
+         );
+
+         List<String> toSelect = Arrays.asList("Books", "Movies, Music & Games", "Health & Beauty", "Sports & Outdoors");
+
+         for (WebElement op : options) {
+             if (op.isDisplayed()) {          
+                 String text = op.getText().trim();
+                 if (toSelect.contains(text)) {
+                     op.click();
+                 }
+             }
+         }
+
+         input.sendKeys(Keys.ENTER);
+
+         driver.quit();
+     }
+}
